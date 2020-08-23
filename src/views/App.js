ad28636c7bc7ccceec043b2a198efdeec772a76c
Denis Ivanov <di@madappgang.com>
2020-08-23 12:23:32 +0300

    fix naming

--- src/views/App.js
+++ src/views/App.js
@@ -7,7 +7,7 @@ import { store, history } from '../redux/store';
 import Login from './Login';
 import Welcome from './Welcome';
 import QuestPage from './QuestPage';
-import SpecialQuest from './SpecialQuest';
+import SpecialQuestPage from './SpecialQuest';
 import NextQuest from './NextQuest';
 import Stages from './Stages';
 import Help from './Help';
@@ -20,7 +20,7 @@ const App = () => {
                 <Route exact path="/" component={Login} />
                 <Route exact path="/welcome" component={Welcome} />
                 <Route exact path="/quest" component={QuestPage} />
-                <Route exact path="/special" component={SpecialQuest} />
+                <Route exact path="/special" component={SpecialQuestPage} />
                 <Route exact path="/next" component={NextQuest} />
                 <Route exact path="/stages" component={Stages} />
                 <Route exact path="/help" component={Help} />
